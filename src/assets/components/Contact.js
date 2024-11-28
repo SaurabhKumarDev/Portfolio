d627f09b9bfbc423ef04edb55a454ddd55fe6aa4
Saurabh <saurabhkumarmehto@gmail.com>
2024-11-28 11:25:21 +0530

    Remove log

--- src/assets/components/Contact.js
+++ src/assets/components/Contact.js
@@ -23,7 +23,7 @@ function Contact() {
         element.target.style.backgroundColor = "black";
         element.target.style.color = "white";
     }
-    console.log(process.env.REACT_APP_CONTACT_API)
+    
     return (
         <div className="text-light container pt-3 pb-3">
             <div>
